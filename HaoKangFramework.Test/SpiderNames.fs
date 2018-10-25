d7f92a3528dbd6761e86b7728180bcb29ad56426
星翼 <853974536@qq.com>
2018-10-25 14:22:24 +0800

    Spiders

---
+++ HaoKangFramework.Test/SpiderNames.fs
@@ -0,0 +1,15 @@
+﻿namespace HaoKangFramework.Test
+
+open System
+open Microsoft.VisualStudio.TestTools.UnitTesting
+open HaoKangFramework
+open System.IO
+open HaoKangFramework.Spiders
+
+[<TestClass>]
+type FrameworkTest () =
+    [<TestMethod>]
+    member x.GetSpiders () =
+        HaoKangFramework.Spider.Spiders
+        |> Array.map (fun (name,factory) -> (name,factory()))
+        |> Array.iter (printfn "%A")
--- HaoKangFramework/Constraints.fs
+++ HaoKangFramework/Constraints.fs
@@ -30,9 +30,22 @@ and ISpider =
     abstract Search : tags : string list -> Page seq
 
 module Spider =
+    open System.Reflection
+
     let inline TestConnection spider =
         (spider :> ISpider).TestConnection ()
     let inline Search param spider =
         (spider :> ISpider).Search param
 
+    let Spiders = 
+        System.Reflection.Assembly.GetExecutingAssembly().GetTypes()
+        |> Array.filter (fun x ->
+            (x |> string).StartsWith "HaoKangFramework.Spiders.")
+        |> Array.filter (fun x ->
+            x.IsClass && 
+            x.IsPublic &&
+            x.GetInterfaces() |> Array.contains typeof<ISpider> &&
+            x.GetConstructors() |> Array.exists (fun x -> x.GetParameters() |> Array.isEmpty))
+        |> Array.map (fun x ->
+            (x.Name,(fun () -> x.Assembly.CreateInstance(x.FullName) :?> ISpider)))
 
